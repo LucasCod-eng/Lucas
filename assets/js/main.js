f4b3612caf3c888ab24230a2afb1ab969fdcdc00
Lucas <lucgabriel002@gmail.com>
2024-05-22 13:34:30 -0300

    reafecture: all

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Desenvolvedor Full Stack"],
+  strings : ["Desenvolvedor Full Stack", "Analista de Sistemas", "Suporte em TI."], 
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
